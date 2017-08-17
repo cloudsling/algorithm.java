c0f567a79dd3c483b9b6726579912842e5ba0fca
Hao Ling <futurelabs@outlook.com>
2017-08-17 15:43:50 +0800

    Insertion sort

---
+++ aug17.sl/src/Insertion.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.lang.*;
+
+public class Insertion {
+    public Insertion() {
+        super();
+
+    }
+
+    public static void sort(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int t = i;
+            for (int j = i - 1; j >= 0; exch(a, t--, j--)) {
+                if (!less(a[t], a[j]))
+                    break;
+            }
+        }
+    }
+
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        sort(args);
+
+        show(args);
+    }
+}
